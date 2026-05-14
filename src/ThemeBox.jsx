f1768a9e6a6ae4312d7297c1963a3161622cbc54
Apfeitoza <annapaulaasf@gmail.com>
2026-05-14 15:43:56 -0300

    refactor: transfer styles to a css file, add data-theme to control changes

--- src/ThemeBox.jsx
+++ src/ThemeBox.jsx
@@ -1,34 +1,21 @@
 import React from "react";
 import ThemeContext from "./ThemeContext";
 
-   const temas = {
-      soft: {
-        nome: "Soft",
-        primaryColor: "#fdf6e7",
-        secondaryColor: "#f4a39d",
-        button: "#f4b16e",
-      },
-      glam: {
-        nome: "Glam",
-        primaryColor: "#3a223f",
-        secondaryColor: "#f6eff6",
-        button: "#b858bf",
-      },
-    };
-const ThemeBox = ({ children }) => {
+ const ThemeBox = ({ children }) => {
   const [tema, setTema] = React.useState("soft");
 
   function trocaTema() {
     setTema((temaAtual) => (temaAtual === "soft" ? "glam" : "soft"));
   }
 
+  //ao inves de usarmos o document.body.style para trocar a cor pelo efeito, só utilizamos o efeito para incluir o data-theme
   React.useEffect(() => {
-    document.body.style.backgroundColor = temas[tema].primaryColor;
+    document.body.setAttribute('data-theme', `${tema}`);
   }, [tema]);
 
   return (
     <ThemeContext.Provider
-      value={{ tema: temas[tema], trocaTema, temaNome: tema }}
+      value={{ tema: tema, trocaTema, temaNome: tema }}
     >
       {children}
     </ThemeContext.Provider>
